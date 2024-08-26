54d7ae7ea09a788ceca4c0d41234a5e22013eb95
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-08-26 21:47:10 +0200

    Add popup.js

---
+++ popup.js
@@ -0,0 +1,13 @@
+const timeElement = document.getElementById("time");
+const currentTime = new Date().toLocaleTimeString();
+
+timeElement.textContent = `The time is: ${currentTime}`;
+
+chrome.action.setBadgeText(
+  {
+    text: "TIME",
+  },
+  () => {
+    console.log("Finished setting badge text.");
+  }
+);
