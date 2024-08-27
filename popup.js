2bf3f772b42b22f3bc97df7c52550cd68c384650
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-08-27 10:12:56 +0200

    Add alarms permission in the manifest

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
 const timeElement = document.getElementById("time");
 const nameElement = document.getElementById("name");
-const currentTime = new Date().toLocaleTimeString();
+const timerElement = document.getElementById("timer");
 
-timeElement.textContent = `The time is: ${currentTime}`;
+function updateTimeElements() {
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0;
+    timerElement.textContent = `The timer is at: ${time} seconds`;
+  });
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `The time is: ${currentTime}`;
+}
 
-chrome.action.setBadgeText(
-  {
-    text: "TIME",
-  },
-  () => {
-    console.log("Finished setting badge text.");
-  }
-);
+updateTimeElements();
+setInterval(updateTimeElements, 1000);
 
 chrome.storage.sync.get(["name"], (res) => {
   const name = res.name;
